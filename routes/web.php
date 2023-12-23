6b51453605643d4fe5125eeeda2b357071bffe20
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-23 11:52:00 +0000

    simple route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing 1',
+                'description' => 'This is the first listing.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing 2',
+                'description' => 'This is the second listing.'
+            ]
+        ]
+    ]);
 });
 
 Route::get('/hello', function () {
