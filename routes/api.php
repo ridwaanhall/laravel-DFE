558964aba93902dad894b1d8a37df5934deb4326
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-23 11:07:14 +0000

    simple api requests

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/posts', function() {
+    return response()->json([
+        'posts' => [
+            [
+                'message' => 'Hello World!'
+            ]
+        ]
+    ]);
+});
+    
+
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
