bac90b5c97ba7eed0df43c7061c49a5f9e3ad922
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-23 12:01:08 +0000

    using unless

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,11 +2,7 @@
     {{ $heading }}
 </h1>
 
-@if (count($listings) == 0)
-    <p>No listings found.</p>
-@else
-
-@endif
+@unless (count($listings) == 0)
 
 @foreach($listings as $listing)
     <h2>
@@ -17,3 +13,9 @@
         {{ $listing['description'] }}
     </p>
 @endforeach
+
+@else
+    <p>
+        No listings found.
+    </p>
+@endunless
